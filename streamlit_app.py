63980087d45b6a2bd898ed2606c2145270765d63
Aravind Yeddula <91241209+Aravindyeddula@users.noreply.github.com>
2022-05-02 17:17:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blackberry Oatmeal')
+streamlit.text('Kale ,spinach & Rocket Smoothie')
